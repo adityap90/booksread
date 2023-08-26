365d43b2b532b03ed036e9abf33019e0c81cb990
adityap90 <adivarma981@gmail.com>
2023-08-26 22:57:46 +0530

    server setup for post request

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,27 @@
+import PostMessage from "../models/postMessages.js";
 
+export const getPosts =  async(req, res) => {
+    // res.send('THIS WORKS ADI');
+    try{
+        const postMessages = await PostMessage.find();
+        // console.log(postMessages)
+        res.status(200).json(postMessages)
+    }
+    catch(error){
+        res.status(401).json({message: error.message})
+    }
+}
 
-export const getPosts =  (req, res) => {
-    res.send('THIS WORKS ADI');
+export const createPost = async(req, res) => {
+ const body = req.body
+ const newPost = new PostMessage(body)
+ try{
+   await newPost.save()
+    // console.log(postMessages)
+    res.status(201).json(newPost)
+}
+catch(error){
+    res.status(409).json({message: error.message})
+}
+
 }
