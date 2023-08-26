3bee723f73582d174667169b30129716f1508ea8
adityap90 <adivarma981@gmail.com>
2023-08-26 23:27:49 +0530

    Frontend code structure-1

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import {Typography, AppBar, Container, Grow, Grid} from '@material-ui/core'
+import memories from './memories.png'
+import Form from './Components/Form/Form'
+import Posts from './Components/Posts/Posts'
 
 function App() {
   return (
-    <div>App</div>
+    <Container maxWidth = "lg">
+      <AppBar position='static' color = "inherit">
+        <Typography variant='h2' align='center'>
+          Memories
+        </Typography>
+        <img src= {memories} alt='memories' height= "500"/>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify = "space-between" alignItems='stretch' spacing={3}>
+            <Grid xs = {12} sm = {7}>
+              <Posts/>
+            </Grid>
+            <Grid xs = {12} sm = {4}>
+              <Form/>
+              </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+
+    </Container>
   )
 }
 
---
+++ client/src/Components/Posts/Posts.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Post from './Post/Post'
+
+function Posts() {
+  return (
+    <>
+    <div>Posts</div>
+    <Post/>
+    <Post/>
+    </>
+    
+  )
+}
+
+export default Posts
