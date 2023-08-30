25e482429e445c210c9c89f7328f50e29dde4168
adityap90 <adivarma981@gmail.com>
2023-08-30 20:59:36 +0530

    redux templates added

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,18 @@ import React from 'react'
 import {Typography, AppBar, Container, Grow, Grid} from '@material-ui/core'
 import memories from './memories.png'
 import Form from './Components/Form/Form'
-import Posts from './Components/Posts/Posts'
+import Posts from './Components/Posts/Posts';
+import useStyles from './styles'
 
 function App() {
+  const classes = useStyles();
   return (
     <Container maxWidth = "lg">
-      <AppBar position='static' color = "inherit">
-        <Typography variant='h2' align='center'>
+      <AppBar className = {classes.appBar} position='static' color = "inherit">
+        <Typography className = {classes.heading} variant='h2' align='center'>
           Memories
         </Typography>
-        <img src= {memories} alt='memories' height= "500"/>
+        <img className= {classes.image} src= {memories} alt='memories' height= "60"/>
       </AppBar>
       <Grow in>
         <Container>
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom';
+import { Provider } from 'react-redux'
+import {configureStore, applyMiddleWare, compose} from 'redux'
+import {thunk} from 'redux-thunk';
+import reducers from'./reducers'
 
 import App from './App';
+
+const store = configureStore(reducers, compose(applyMiddleWare(thunk)));
 
 ReactDOM.render(<App/>, document.getElementById('root'))
