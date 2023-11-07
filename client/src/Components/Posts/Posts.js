bb36753b7bdd72bea6be2132a59c3cb5101c118a
adityap90 <adivarma981@gmail.com>
2023-11-07 19:15:09 +0530

    books per Books details Added

--- client/src/Components/Posts/Posts.js
+++ client/src/Components/Posts/Posts.js
@@ -1,20 +1,25 @@
-import React from 'react'
-import Post from './Post/Post'
-import useStyles from './Styles';
+import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
-function Posts() {
-  const classes = useStyles();
-  const posts1 = useSelector((state) => state.posts);
-  
-  return (
-    <>
-    <div>Posts</div>
-    <Post/>
-    <Post/>
-    </>
-    
-  )
-}
+import Post from './Post/Post';
+import useStyles from './Styles';
 
-export default Posts
+const Posts = ({ setCurrentId }) => {
+  const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
+
+  return (
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6} md={6}>
+            <Post post={post} setCurrentId={setCurrentId} />
+          </Grid>
+        ))}
+      </Grid>
+    )
+  );
+};
+
+export default Posts;
